a1208317ae15bd2c62bb21302e3864b8f0c55b96
ekecardinal <ekecardinal@gmail.com>
2025-11-28 17:38:31 +0100

    added correction

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,14 +1,16 @@
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
+import { getUser } from "../action/getUser";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const user = await getUser();
   return (
     <div>
-      <Navbar />
+      <Navbar user={user} />
       {children}
       <Footer />
     </div>
